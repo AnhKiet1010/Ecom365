f576c41c27a30a687fb742ad22943ac3dc190067
anhkiet <letrananhkiet1010@gmail.com>
2022-06-13 14:29:24 +0700

    fix lint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    reactRefresh(),
+    tsconfigPaths(),
+    svgr(),
+    vitePluginImp({
+      optimize: true,
+      libList: [
+        {
+          libName: 'antd',
+          libDirectory: 'es',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#49ADFC' },
+      },
+    },
+  },
 })
